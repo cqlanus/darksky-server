fda6fae002f4082a13204f930a0b0283e2819722
Chris Lanus <chris.lanus@isobar.com>
2019-07-17 16:29:54 -0500

    updating origin

--- index.js
+++ index.js
@@ -3,21 +3,44 @@ const fetch = require("node-fetch")
 const app = express()
 const cors = require("cors")
 const morgan = require("morgan")
-const https = require('https')
-const fs = require('fs')
-const querystring = require('querystring')
+const https = require("https")
+const fs = require("fs")
+const querystring = require("querystring")
 const port = process.env.PORT || 5000
 
-const getSkewTData = require('./utils/skewt')
+const getSkewTData = require("./utils/skewt")
 
 const DARK_SKY_KEY = process.env.DARK_SKY_KEY
 const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN
 const lat = "41.8781"
 const lng = "-87.6298"
 const position = `${lat},${lng}`
-const url = (pos = position) => `https://api.darksky.net/forecast/ef48a877c2d39af5dac158b383af6fa8/${pos}?extend=hourly`
+const url = (pos = position) =>
+    `https://api.darksky.net/forecast/ef48a877c2d39af5dac158b383af6fa8/${pos}?extend=hourly`
+
+const whitelist = [
+    "https://localhost:3000",
+    "https://master.d2374hhhwbye5z.amplifyapp.com",
+    "https://weather.chrislanus.com",
+]
+
+const getOrigin = (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+    } else {
+        callback(new Error("Not allowed by CORS"))
+    }
+}
 
 const request = async (url, params) => {
+    const headers = {
+        "Access-Control-Allow-Origin":
+            "https://master.d2374hhhwbye5z.amplifyapp.com",
+        "Access-Control-Allow-Credential": true,
+        mode: "cors",
+        credentials: "omit",
+        ...params,
+    }
     const res = await fetch(url, params)
     return await res.json()
 }
@@ -32,13 +55,25 @@ const errorHandler = (err, req, res, next) => {
     res.render("error", { error: err })
 }
 
-var corsOptions = {
-    origin: isDev() ? "https://localhost:3000" : "https://master.d2374hhhwbye5z.amplifyapp.com/"
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+const corsOptions = {
+    origin: (origin, callback) => {
+        console.log({origin})
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
 }
 
 app.use(cors(corsOptions))
 app.use(morgan("tiny"))
+
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+})
 
 app.get("/forecast", (req, res, next) => {
     const headers = {
@@ -53,7 +88,7 @@ app.get("/forecast", (req, res, next) => {
     const { latitude, longitude } = req.query
     const pos = latitude && longitude ? `${latitude},${longitude}` : position
     const dynamicUrl = url(pos)
-    console.log({dynamicUrl})
+    console.log({ dynamicUrl })
     request(dynamicUrl, params)
         .then(data => {
             res.send(data)
@@ -74,20 +109,24 @@ app.get("/geocode", (req, res, next) => {
         .catch(next)
 })
 
-app.get('/reversegeocode', (req, res, next) => {
+app.get("/reversegeocode", (req, res, next) => {
     const baseUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
     const { latitude, longitude } = req.query
     const queryParam = `${longitude},${latitude}`
 
     const url = `${baseUrl}${queryParam}.json?access_token=${MAPBOX_ACCESS_TOKEN}`
-    request(url)
+    const params = {
+        mode: "cors",
+        credentials: "omit",
+    }
+    request(url, params)
         .then(data => {
             res.send(data)
         })
         .catch(next)
 })
 
-app.get('/skewt', (req, res, next) => {
+app.get("/skewt", (req, res, next) => {
     const { airport } = req.query
     getSkewTData(airport)
         .then(data => {
@@ -106,4 +145,6 @@ app.use(function(err, req, res, next) {
 //     cert: fs.readFileSync('./server.cert')
 // }, app)
 
-app.listen(port, () => console.log(`App listening on port ${port}! ENV: ${process.env.NODE_ENV}`))
+app.listen(port, () =>
+    console.log(`App listening on port ${port}! ENV: ${process.env.NODE_ENV}`)
+)
